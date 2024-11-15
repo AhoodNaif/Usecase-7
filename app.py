3f807f359b8a712adaee00bf1278a29dc49e8e41
ohoud naif <ohoudalnaif@yahoo.com>
2024-11-15 11:16:25 +0300

    Ahood

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ st.title("Football Price Range Prediction")
 
 # User inputs
 age = st.number_input("Age", min_value=15, max_value=35, value=30)
+goals=st.number_input("Goals",min_value=0,max_value=50,value=0)
+goals_conceded=st.number_input("goals conceded",min_value=0,max_value=50,value=0)
+clean_sheets=st.number_input("clean sheets",min_value=0,max_value=50,value=0)
+minutes_played=st.number_input("minutes played",min_value=0,max_value=50,value=0)
+winger=st.number_input("winger",min_value=0,max_value=50,value=0)
+red_cards=st.number_input("red cards",min_value=0,max_value=50,value=0)
+yellow_cards=st.number_input("yellow cards",min_value=0,max_value=50,value=0)
 assists = st.number_input("Assists", min_value=0, max_value=20, value=1)
 days_injured = st.number_input("Days Injured", min_value=0, max_value=500, value=1)
 award = st.number_input("Awards", min_value=0, max_value=500, value=1)
@@ -29,12 +36,20 @@ if st.button("Predict Price Range"):
     try:
         response = requests.post(url, json=data)
         response.raise_for_status()  # Check for request errors
-        prediction = response.json()  # Parse JSON response
         
-        # Interpret the prediction
-        price_range = {0: "Cheap Price", 1: "Good Price", 2: "High Price"}
-        st.write(f"Estimated Price: {price_range[prediction['pred']]}")
+        # Check if 'pred' exists in the response JSON
+        if "pred" in response.json():
+            prediction = response.json()["pred"]
+            
+            # Interpret the prediction
+            price_range = {0: "Cheap Price", 1: "Good Price", 2: "High Price"}
+            st.write(f"Estimated Price: {price_range.get(prediction, 'Unknown Price Range')}")
+        else:
+            st.error("Prediction not found in API response.")
         
     except requests.exceptions.RequestException as e:
         st.error("Error requesting prediction from API. Please try again.")
         st.write(e)
+    except KeyError as e:
+        st.error("Unexpected response format.")
+        st.write(f"Error: {e}")
